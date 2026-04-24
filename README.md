c58b153ad299766b3b7ba062bd7f698196747be7
Samestora <putrantosurya38@gmail.com>
2026-04-24 16:16:53 +0700

    Feat : Tambahan serial, interrupt, dan potensio

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 | Raditya Yusuf Ramadhan       | H1D023057  |
 
 ## Deskripsi Projek
-Sistem ini merupakan alarm keamanan berbasis Arduino yang menggunakan PIR sensor untuk mendeteksi gerakan. Ketika gerakan terdeteksi, sistem akan mengaktifkan buzzer sebagai alarm, menyalakan LED sebagai indikator visual, serta menampilkan pesan peringatan pada LCD melalui komunikasi I2C. Data status juga dikirim melalui komunikasi serial untuk monitoring.
+Sistem ini merupakan alarm keamanan berbasis Arduino yang menggunakan PIR sensor untuk mendeteksi gerakan. Ketika gerakan terdeteksi, sistem akan mengaktifkan buzzer sebagai alarm, menyalakan LED sebagai indikator visual, serta menampilkan pesan peringatan pada LCD melalui komunikasi I2C. Data status juga dikirim melalui komunikasi serial untuk monitoring. Ketika tombol ditekan maka system tidak lagi membaca, sangat berguna apabila ingin mematikan alarm. Suara buzzer bisa disetting dengan potensiometer
 
 ## Komponen
 - Arduino UNO R3
@@ -21,6 +21,8 @@ Sistem ini merupakan alarm keamanan berbasis Arduino yang menggunakan PIR sensor
 - LED
 - LCD 16x2 I2C PCF8574-based, 39 (0x27)
 - Resistor 1k Ω
+- Push button
+- Potensiometer
 
 ## Cara Kerja
 Sistem akan mendeteksi gerakan menggunakan PIR sensor.
@@ -29,6 +31,7 @@ Jika ada gerakan:
 - LED menyala
 - LCD menampilkan peringatan
 - Status dikirim ke Serial Monitor
+- Jika perlu matikan dengan push button (dan sebaliknya) dan suara buzzer dengan potensiometer 
 
 ## Rangkaian
 ![](images/schematics.png)
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,31 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
+#define BUZZER 2
+#define PIR 7
+#define BUTTON 3
+#define POT A0
+
+volatile bool systemActive = true;
+
+void toggleSystem() {
+  systemActive = !systemActive;
+}
+
 void setup(){
-  pinMode(2, OUTPUT);
-  pinMode(7, INPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(PIR, INPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON), toggleSystem, FALLING);
+
+  Serial.begin(9600);
+
   lcd.init();
   lcd.backlight();
+
   lcd.setCursor(0,0);
-  lcd.print(" DETEKSI GERAK  ");
+  lcd.print(" DETEKSI GERAK ");
   lcd.setCursor(0,1);
   lcd.print("READY");
   delay(2000);
@@ -16,21 +34,42 @@ void setup(){
 }
 
 void loop(){
-  int gerak = digitalRead(7);
+  int gerak = digitalRead(PIR);
+  int nilaiADC = analogRead(POT);
+  int frek = map(nilaiADC, 0, 1023, 500, 2000);
 
-  if (gerak > 0) {
+  // ===== SERIAL MONITOR =====
+  Serial.print("System: ");
+  Serial.print(systemActive ? "ON" : "OFF");
+  Serial.print(" | Gerak: ");
+  Serial.print(gerak ? "ADA" : "AMAN");
+  Serial.print(" | ADC: ");
+  Serial.print(nilaiADC);
+  Serial.print(" | Frek: ");
+  Serial.println(frek);
+
+  // ===== LOGIKA UTAMA =====
+  if (systemActive) {
+    if (gerak > 0) {
+      lcd.setCursor(0,0);
+      lcd.print("INFO          ");
+      lcd.setCursor(0,1);
+      lcd.print("Ada maling!   ");
+      tone(BUZZER, frek);
+    } else {
+      lcd.setCursor(0,0);
+      lcd.print("INFO          ");
+      lcd.setCursor(0,1);
+      lcd.print("Aman          ");
+      noTone(BUZZER);
+    }
+  } else {
     lcd.setCursor(0,0);
-    lcd.print("INFO");
+    lcd.print("SYSTEM OFF    ");
     lcd.setCursor(0,1);
-    lcd.print("ada maling weh");
-    tone(2,1500,500);
+    lcd.print("Alarm mati    ");
+    noTone(BUZZER);
   }
-  else {
-    lcd.setCursor(0,0);
-    lcd.print("INFO");
-    lcd.setCursor(0,1);
-    lcd.print("Aman");
-    noTone(2);
-  }
+
   delay(500);
 }
